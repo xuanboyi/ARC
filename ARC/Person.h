b424733543b2a366cc7075822ffe66571a5fd1ac
玄博屹 <genhiroshi@xuanboyideMacBook-Pro.local>
2017-12-13 22:07:36 +0800

    修改代码

---
+++ ARC/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  ARC
+//
+//  Created by 玄博屹 on 2017/12/13.
+//  Copyright © 2017年 玄博屹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ ARC/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  ARC
+//
+//  Created by 玄博屹 on 2017/12/13.
+//  Copyright © 2017年 玄博屹. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+-(void)dealloc{
+    NSLog(@"我被释放了");
+}
+
+
+@end
